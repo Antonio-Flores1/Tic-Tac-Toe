a616cf32c36c2f5422f7fa1744cdbc866fb04f0c
Antonio-Flores1 <antonioflores2280@gmail.com>
2023-07-06 20:03:05 -0400

    Board populates with naught and crosses with Turn label being updated with current palyer's turn, boardFull func created to determine of board is empty or Full

--- tictactoe/ViewController.swift
+++ tictactoe/ViewController.swift
@@ -35,17 +35,50 @@ class ViewController: UIViewController {
     @IBOutlet weak var c2: UIButton!
     @IBOutlet weak var c3: UIButton!
     
+    var buttonBoard = [UIButton]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        initBoard()
+        buttonBoard = [a1,a2,a3,b1,b2,b3,c1,c2,c3]
     }
 
     @IBAction func boardTapAction(_ sender: UIButton) {
-        
+        addToBoard(sender)
+        print(boardFull())
+    }
+    
+    
+    func initBoard(){
         
     }
     
+    func boardFull() -> Bool {
+        for button in buttonBoard {
+            if button.title(for: .normal) == nil {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func addToBoard(_ sender: UIButton) {
+        
+        if (sender.title(for: .normal) == nil ){
+            if (currentTurn == Turn.Naught) {
+                sender.setTitle(naught, for: .normal)
+                currentTurn = Turn.Cross
+                turnLabel.text = cross
+            } else if (currentTurn == Turn.Cross) {
+                sender.setTitle(cross, for: .normal)
+                currentTurn = Turn.Naught
+                turnLabel.text = naught
+            }
+            sender.isEnabled = false
+        }
+//
+    }
     
     
 }
